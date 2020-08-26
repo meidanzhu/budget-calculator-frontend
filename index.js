9e9e98ede447ec38ee8d7f8c476a90035ca249df
meidanzhu <meidan.zhu@gmail.com>
2020-08-26 01:07:51 -0400

    making fetch request

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-document.addEventListener('DOMContentLoaded', ()=> alert('hi'))
+//document.addEventListener('DOMContentLoaded', ()=> alert('hi'))
+expenseList = document.getElementById('expense-list')
+
+function fetchExpenses(){
+    fetch('http://127.0.0.1:3000/expenses')
+    .then(res => res.json())
+    .then(addExpensesToDom)
+}
+
+function addExpensesToDom(response){
+    debugger
+    response.data.forEach( item => {
+        addItemToDom(item)
+         })
+}
+
+fetchExpenses()
